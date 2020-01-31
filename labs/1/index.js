6ad7debb60d238ed17dea3eb7a1cf6745d1d5219
NathanS97 <42750605+nSnod@users.noreply.github.com>
2020-01-30 20:33:49 -0500

    lab 1 commit

---
+++ labs/1/index.js
@@ -0,0 +1,8 @@
+const Adder = require("./Adder.js");
+
+let adder = new Adder({
+  a: 24,
+  b: 45
+});
+
+console.log(adder.render());
---
+++ labs/1/readme.md
@@ -0,0 +1,3 @@
+# Lab 1: ES6 Classes and Node
+
+This lab features basic addition of variables using classes and Node.
