0459188b997cf1bfdc30279d1ea5f85ed9f9d765
NathanS97 <42750605+nSnod@users.noreply.github.com>
2020-02-03 12:10:55 -0500

    Co-authored-by: Gab-Sinclair <gabrielle23@knights.ucf.edu> Co-authored-by: leidyp96 <leidypulido96@gmail.com> Co-authored-by: eguffey1 <eguffeyarmy2@gmail.com>

--- ice/w5a/src/index.js
+++ ice/w5a/src/index.js
@@ -1,16 +1,26 @@
-function runOnLoad()
-{
-    // Create a container for us
-    let element = document.createElement("div");
-    element.id = "container";
-    document.body.appendChild(element);
+import Task from "./components/Task.js";
 
-    // Handle adding a new task
-    var addTaskButton = document.getElementById("addTask");
-    addTaskButton.addEventListener("click", onClick)
+let element;
+let inputElement;
+function runOnLoad() {
+  // Create a container for us
+  element = document.createElement("div");
+  element.id = "container";
+  document.body.appendChild(element);
+
+  // Handle adding a new task
+  var addTaskButton = document.getElementById("addTask");
+  inputElement = document.getElementById("taskText");
+  addTaskButton.addEventListener("click", onClick);
 }
+
+//Create new div for new task
 function onClick() {
-    console.log("clicked!");
+  console.log("clicked!");
+  var newTask = new Task({
+    content: inputElement.value
+  });
+  element.appendChild(newTask.render());
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
