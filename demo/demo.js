e4e5be86be88ad48234a0c711b08ad80f28ebbcf
NathanS97 <42750605+nSnod@users.noreply.github.com>
2020-02-12 13:18:47 -0500

    w6b eslint commit

--- demo/demo.js
+++ demo/demo.js
@@ -1,60 +1,60 @@
 class Pet {
-  constructor(props) {
-    this.props = props;
+  constructor (props) {
+    this.props = props
   }
 
-  getName() {
-    return this.props.name;
+  getName () {
+    return this.props.name
   }
 
-  getSpecies() {
-    return this.props.species;
+  getSpecies () {
+    return this.props.species
   }
 
-  getSpeech() {
-    return "I make this sound: ";
+  getSpeech () {
+    return 'I make this sound: '
   }
 
   getIdentity = () => {
-    console.log(this);
+    console.log(this)
   };
 }
 
 class Cat extends Pet {
-  constructor(props) {
-    super(props);
-    this.props.species = "cat";
+  constructor (props) {
+    super(props)
+    this.props.species = 'cat'
   }
 
-  getSpeech() {
-    return super.getSpeech() + "Meow!";
+  getSpeech () {
+    return super.getSpeech() + 'Meow!'
   }
 }
 
 class Dog extends Pet {
-  constructor(props) {
-    super(props);
-    this.props.species = "dog";
+  constructor (props) {
+    super(props)
+    this.props.species = 'dog'
   }
 
-  getSpeech() {
-    return super.getSpeech() + "Woof!";
+  getSpeech () {
+    return super.getSpeech() + 'Woof!'
   }
 }
 
-function sayName(pet) {
-  console.log(`My name is ${pet.getName()}, and I am a ${pet.getSpecies()}`);
-  console.log(`${pet.getSpeech()}`);
+function sayName (pet) {
+  console.log(`My name is ${pet.getName()}, and I am a ${pet.getSpecies()}`)
+  console.log(`${pet.getSpeech()}`)
 }
 
-var myPet = new Cat({ name: "Luna" });
-sayName(myPet);
-myPet.getIdentity();
+var myPet = new Cat({ name: 'Luna' })
+sayName(myPet)
+myPet.getIdentity()
 // Assign method getIdentity to the variable myIdentity
-var myIdentity = myPet.getIdentity();
-//Call the new function
-myIdentity();
+var myIdentity = myPet.getIdentity()
+// Call the new function
+myIdentity()
 
-var myDog = new Dog({ name: "Fido" });
-sayName(myDog);
-myDog.getIdentity();
+var myDog = new Dog({ name: 'Fido' })
+sayName(myDog)
+myDog.getIdentity()
