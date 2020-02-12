07a1e26a0c02b5ef5ba252c6f9a62a501c0da0d5
NathanS97 <42750605+nSnod@users.noreply.github.com>
2020-02-12 11:45:43 -0500

    Updates

---
+++ precache-manifest.504e6fe8bb1099f925fffd75a761e106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36514c0b1f4a9711f57d85c293bd85c1",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "95fee7f615f64e5f7695",
+    "url": "/dig4639-mobile-dev/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fee7f615f64e5f7695",
+    "url": "/dig4639-mobile-dev/static/js/main.95ef6fb5.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
