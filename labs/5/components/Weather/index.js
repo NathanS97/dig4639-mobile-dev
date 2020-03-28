a8642d6bf6ea854cc41e743b709cff4315fb2797
NathanS97 <42750605+nSnod@users.noreply.github.com>
2020-03-28 18:39:52 -0400

    lab 5

---
+++ labs/5/components/Weather/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  card: {
+
+    justifyContent: "center",
+    backgroundColor: "#add8e6",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "6px",
+    alignItems: "center",
+    textAlign: "center",
+    width: "80%",
+    padding: "1em",
+    borderWidth: 3,
+    borderColor: "darkblue",
+    borderRadius: 2
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold"
+  }
+})
+
+class Weather extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      periods: []
+    };
+
+  }
+
+  componentDidMount() {
+
+    fetch("https://api.weather.gov/gridpoints/MLB/25,69/forecast")
+      .then(res => res.json())
+      .then((result) => {
+        let periods = result.properties.periods;
+
+        this.setState({
+          periods: periods
+        });
+
+      })
+      .catch((error) => { console.log(error) });
+
+  }
+
+  render() {
+    return (
+      <View>
+        {
+          this.state.periods.map((value, index) => {
+            return <View style={styles.card} key={index}>
+              <Text style={styles.heading}> {value.name}</Text>
+              <Text style={styles.heading}>{value.temperature}{value.temperatureUnit}</Text>
+              <Text>{value.detailedForecast}</Text>
+            </View>;
+          })
+
+        }
+      </View>
+    );
+  }
+
+}
+
+export default Weather;
