3defd62d93f5b56655738436fc009e2a0b7c104f
NathanS97 <42750605+nSnod@users.noreply.github.com>
2020-04-13 16:50:04 -0400

    final proj initial commit, in progress

---
+++ project/contact-app/src/components/Contacts/Delete/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Delete extends React.Component {
+  render() {
+    return (
+      <div>
+        Delete class Working
+      </div>
+    );
+  }
+}
+
+export default Delete;
